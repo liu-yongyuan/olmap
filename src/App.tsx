6f27b56bac94d6d26de684bc3ffd5120a4126adb
Matt Liu <liuyychn@outlook.com>
2025-05-27 18:43:34 +0800

    提交代码

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MapComponent from "./components/map";
 export default function App() {
   return (
      <div className="min-h-screen p-4 bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">OpenLayers + ol-ext Demo</h1>
+      <h1 className="text-2xl font-bold mb-4">OpenLayers</h1>
       <MapComponent />
     </div>
   );
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -7,31 +7,63 @@ import View from "ol/View";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
 import { OSM } from "ol/source";
 import VectorSource from "ol/source/Vector";
-import { Style, Stroke, Fill } from "ol/style";
-import { Polygon } from "ol/geom";
+import { Style, Stroke, Fill, RegularShape } from "ol/style";
+import { Circle, LineString, Point, Polygon } from "ol/geom";
 import Feature from "ol/Feature";
 import { fromExtent } from "ol/geom/Polygon";
 import Transform from "ol-ext/interaction/Transform";
+import { shiftKeyOnly } from "ol/events/condition";
 
 const MapComponent = () => {
   const mapRef = useRef(null);
 
   useEffect(() => {
-    // 矢量图层
-    const rect = fromExtent([0, 0, 500000, 500000]);
-    const feature = new Feature(rect);
+    // Style
+    const getStyle = (feature: any) => {
+      return [
+        new Style({
+          image: new RegularShape({
+            fill: new Fill({ color: [0, 0, 255, 0.4] }),
+            stroke: new Stroke({ color: [0, 0, 255, 1], width: 1 }),
+            radius: feature.get("radius") || 10,
+            points: 3,
+            angle: feature.get("angle") || 0,
+          }),
+          fill: new Fill({ color: [0, 0, 255, 0.4] }),
+          stroke: new Stroke({ color: [0, 0, 255, 1], width: 1 }),
+        }),
+      ];
+    };
 
-    feature.setStyle(
-      new Style({
-        stroke: new Stroke({ color: "red", width: 2 }),
-        fill: new Fill({ color: "rgba(255, 0, 0, 0.1)" }),
-      })
-    );
-
-    const vectorSource = new VectorSource({ features: [feature] });
+    const vectorSource = new VectorSource({
+      features: [
+        new Feature(
+          new Polygon([
+            [
+              [34243, 6305749],
+              [-288626, 5757848],
+              [210354, 5576845],
+              [300000, 6000000],
+              [34243, 6305749],
+            ],
+          ])
+        ),
+        new Feature(
+          new LineString([
+            [406033, 5664901],
+            [689767, 5718712],
+            [699551, 6149206],
+            [425601, 6183449],
+          ])
+        ),
+        new Feature(new Point([269914, 6248592])),
+        new Feature(new Circle([500000, 6400000], 100000)),
+      ],
+    });
 
     const vectorLayer = new VectorLayer({
       source: vectorSource,
+      style: getStyle,
     });
 
     // 地图实例
@@ -52,11 +84,29 @@ const MapComponent = () => {
     // ol-ext Transform
     const transform = new Transform({
       layers: [vectorLayer],
-      enableRotatedTransform: true,
-      addCondition: () => true,
+      enableRotatedTransform: false,
+      addCondition: shiftKeyOnly,
+      hitTolerance: 2,
+      translateFeature: false,
+      scale: true,
+      rotate: true,
+      keepAspectRatio: undefined,
+      keepRectangle: false,
+      translate: true,
+      stretch: true,
+      // Get scale on points
+      pointRadius: function (f) {
+        var radius = f.get("radius") || 10;
+        return [radius, radius];
+      },
     });
 
     map.addInteraction(transform);
+
+    // setTimeout(() => {
+    //   map.removeInteraction(transform);
+    //   map.removeLayer(vectorLayer);
+    // }, 10 * 1000);
 
     return () => map.setTarget(undefined);
   }, []);
